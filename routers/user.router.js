0e21d526b18d489d330cfd0c60529b2441b73755
kdevkh <kdevkh@gmail.com>
2024-02-07 18:40:26 +0900

    [Feat] user.router in progress && added createdAt and updatedAt to tables in shcema.prisma

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,4 +2,26 @@ import express from 'express';
 
 const router = express.Router();
 
+router.post('/sign-up', (Req, res, next) => {
+  const { email, password, passwordConfirm, name } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: '이메일은 필수값입니다.' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: '비밀번호는 필수값입니다.' });
+  }
+  if (!passwordConfirm) {
+    return res.status(400).json({ message: '비밀번호 확인은 필수값입니다.' });
+  }
+  if (!name) {
+    return res.status(400).json({ message: '이름은 필수값입니다.' });
+  }
+  if (password.email < 6) {
+    return res.status(400).json({ message: '비밀번호는 최소 6자 이상입니다.' });
+  }
+  if (password !== passwordConfirm) {
+    return res.status(400).json({ message: '비밀번호가 일치하지 않습니다.' });
+  }
+});
+
 export default router;
