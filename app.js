bc23d0011778addc77e5a1c4e2b38f394410a43e
kdevkh <kdevkh@gmail.com>
2024-02-07 18:15:00 +0900

    [Feat] routers divided & app.js added

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import userRouter from './routers/user.router.js';
+import postRouter from './routers/post.router.js';
+import commentRouter from './routers/comment.router.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cookieParser());
+
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
+
+app.listen(PORT, () => {
+  console.log(`sparpet app listening on port ${PORT}`);
+});
